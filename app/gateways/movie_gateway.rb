585ca2cbfffae6636718f012f2e7e71a8d8eb265
Paul Knapp <ppankluap@gmail.com>
2025-02-07 22:32:16 -0600

    Feat: endpoint still broken... moving on

--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -5,16 +5,22 @@ class MovieGateway
     BASE_URL = 'https://api.themoviedb.org/3'
 
     def self.get_top_rated_movies
-        response = connection.get("/movie/popular") do |req|
-          req.params = {
-            api_key: Rails.application.credentials.tmdb[:api_key],
-            page: 1
+        response = connection.get("/movie/top_rated") do |req|
+          req.params = { 
+            page: 1,
+            language: 'en-US',
+            sort_by: 'popularity.desc',
+            api_key: Rails.application.credentials.tmdb[:api_key]
           }
         end
+ 
+        puts "Request URL: #{response.env.url}" #should show me url being called
         if response.status == 200 
             JSON.parse(response.body)['results']
+        elsif response.status == 404
+            raise StandardError, "Resource not found status: #{response.status}"
         else
-            []
+            raise StandardError, "Unexpected response status: #{response.status}"
         end
        
     end
@@ -30,11 +36,14 @@ class MovieGateway
         end
         JSON.parse(response.body)['results']
     end
+
+
       private
 
 
     def self.connection 
         Faraday.new(url: BASE_URL) do |faraday|
+            faraday.response :logger #should theoretically log the request
             faraday.adapter Faraday.default_adapter
         end
     end
