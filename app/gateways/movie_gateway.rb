6342cebbeca7df0a8cf88bd1d3e442059bdd34bb
Paul Knapp <ppankluap@gmail.com>
2025-02-07 21:48:55 -0600

    Feat: api call broken. will investigate more as I go.

--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -1,26 +1,40 @@
 require 'faraday'
-
+require 'json'
 class MovieGateway
 
-    BASE_URL = 'https://api.themovietb.org/3'
+    BASE_URL = 'https://api.themoviedb.org/3'
 
     def self.get_top_rated_movies
-        response = connection.get("/movie/top_rated") do |req|
+        response = connection.get("/movie/popular") do |req|
           req.params = {
             api_key: Rails.application.credentials.tmdb[:api_key],
             page: 1
           }
         end
-        JSON.parse(response.body)['results']
+        if response.status == 200 
+            JSON.parse(response.body)['results']
+        else
+            []
+        end
+       
     end
 
 
-
+    def self.search_movies(query)
+        response = connection.get("/search/movie") do |req|
+          req.params = {
+            api_key: Rails.application.credentials.tmdb[:api_key],
+            query: query,
+            page: 1
+          }
+        end
+        JSON.parse(response.body)['results']
+    end
       private
 
 
     def self.connection 
-        faraday.new(url: BASE_URL) do |faraday|
+        Faraday.new(url: BASE_URL) do |faraday|
             faraday.adapter Faraday.default_adapter
         end
     end
